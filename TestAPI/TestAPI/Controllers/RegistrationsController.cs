3a70c86fcf793942967af0d2e9d1bef40e43f876
HaiMinhNguyenInfodation <85538079+HaiMinhNguyenInfodation@users.noreply.github.com>
2022-03-30 16:39:28 +0700

    fix: remove obj and debug

---
+++ TestAPI/TestAPI/Controllers/RegistrationsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+    [Route("/v1/registration")]
+    [ApiController]
+    public class RegistrationsController : ControllerBase
+    {
+
+        private readonly ILogger<RegistrationsController> _logger;
+        public RegistrationsController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<RegistrationsController>();
+        }
+
+        [HttpPost]
+        [Route("step-1")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+
+        public async Task<IActionResult> Register([FromBody] RegistrationStep1RequestDto registrationStep1RequestDto)
+        {
+            try
+            {
+                _logger.LogInformation($"Start call api register step 1 with [Email = {registrationStep1RequestDto.Email}, Screen name = {registrationStep1RequestDto.ScreenName}].....");
+                //var response = await _registrationService.SetStep1Async(registrationStep1RequestDto);
+                _logger.LogInformation($"Create account for [Email = {registrationStep1RequestDto.Email}, Screen name = {registrationStep1RequestDto.ScreenName}] ");
+                return Ok();
+            }
+            catch (ArgumentException ex)
+            {
+                _logger.LogError(ex, ex.Message.ToString());
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Fail to register");
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
---
+++ TestAPI/TestAPI/Models/RegistrationStep1RequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class RegistrationStep1RequestDto
+    {
+        public string ScreenName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Language { get; set; } = "en";
+    }
+}
